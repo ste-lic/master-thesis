202932b6caa45bd35cb2e7fdcd6bc096ad76f689
Stefano Licinio <73396887+ste-lic@users.noreply.github.com>
2026-05-20 15:37:29 +0200

    Add BLE functionality for ESP32-C3

---
+++ ble-fuzz/esp32c3_notify.ino
@@ -0,0 +1,65 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      Serial.println("Device connected!");
+      
+      pCharacteristic->setValue("Connection acknowledged. Hello from ESP32-C3!");
+      pCharacteristic->notify();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.println("Device disconnected!");
+      BLEDevice::startAdvertising();
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE...");
+
+  BLEDevice::init("ESP32-C3_Device");
+  
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+  pCharacteristic->setValue("Waiting for connection...");
+  
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMaxPreferred(0x12);
+  BLEDevice::startAdvertising();
+  
+  Serial.println("BLE device is ready and advertising");
+  Serial.println("Device name: ESP32-C3_Device");
+}
+
+void loop() {
+  delay(1000);
+}
